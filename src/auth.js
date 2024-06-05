f37ef6cd8157bc152adf35f0975641f32ac5c8ac
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-06-05 18:36:25 +1000

    edited adminAuthRegister function

--- src/auth.js
+++ src/auth.js
@@ -1,11 +1,12 @@
 
 /**
- * Cool function doing stuff :)
- * @param {*} email 
- * @param {*} password 
- * @param {*} nameFirst 
- * @param {*} nameLast 
- * @returns 
+ * Register a user with an email, password, and names, then returns their authUserId value.
+ * @param {string} email - auth email
+ * @param {string} password - auth password
+ * @param {string} nameFirst - auth first name
+ * @param {string} nameLast - auth last name
+ * 
+ * @returns {{authUserId}} - return object
  */
 
 function adminAuthRegister(email, password, nameFirst, nameLast) {
