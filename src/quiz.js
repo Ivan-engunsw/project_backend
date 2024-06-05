3edc566dc038d4c7a08eb1a36775553020c6a159
Dzan Gwan Alan Au <z5308353@ad.unsw.edu.au>
2024-06-05 18:58:15 +1000

    Implemented stub function for adminQuizNameUpdate in quiz.js

---
+++ src/quiz.js
@@ -0,0 +1,12 @@
+/**
+ * Update the name of the relevant quiz.
+ *
+ * @param {int} authUserId - authorised user Id
+ * @param {int} quizId - quiz Id
+ * @param {string} name - name of quiz
+ * @returns {{}} - empty object
+ */
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {
+    };
+}
