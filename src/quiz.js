de9600af27ae88272654468798da6b8d6e856e7d
Ivan Lun Hui Chen <z5557064@ad.unsw.edu.au>
2024-06-05 19:31:39 +1000

    Added stub adminQuizList function in quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -1,4 +1,19 @@
 /**
+ * Provide a list of all quizzes that are owned by the currently logged in user.
+ * @param {number} authUserId - authorised user Id
+ * @returns {{quizId,name}} - object containing quizId and name
+ */
+function adminQuizList(authUserId) {
+    return { quizzes: [
+        {
+          quizId: 1,
+          name: 'My Quiz',
+        }
+      ]
+    }
+}
+
+/**
  * Update the name of the relevant quiz.
  *
  * @param {int} authUserId - authorised user Id
