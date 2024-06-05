203c17728498c01bba413a5ccfdc64e134e9a1bb
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-06-05 19:23:39 +1000

    Created adminUserDetails stub function

--- src/auth.js
+++ src/auth.js
@@ -27,3 +27,23 @@ function adminAuthLogin(email, password) {
     authUserId: 1,
   }
 }
+
+/**
+ * Given an admin user's authUserId, return details about the user.
+    "name" is the first and last name concatenated with a single space between them.
+ * @param {integer} authUserId - auth email
+ * 
+ * @returns {{user}} - return object
+ */
+
+function adminUserDetails(authUserId) {
+  return { user:
+    {
+      userId: 1,
+      name: 'Hayden Smith',
+      email: 'hayden.smith@unsw.edu.au',
+      numSuccessfulLogins: 3,
+      numFailedPasswordsSinceLastLogin: 1,
+    }
+  };
+}
