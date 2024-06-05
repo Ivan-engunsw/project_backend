f2476d7759cba1a1cde8bf89610f1b53cea9e1b6
z5479195 <z5479195@ad.unsw.edu.au>
2024-06-05 19:10:43 +1000

    added adminQuizInfo stub to quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -7,3 +7,19 @@
 function adminQuizRemove(authUserId, quizId) {
     return {};
 }
+
+/**
+ * Get all of the relevant information about the current quiz.
+ * @param {integer} authUserId 
+ * @param {integer} quizId 
+ * @returns {object}
+ */
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
