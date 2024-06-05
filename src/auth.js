b462b4840678abb3c363396de26b160912c4e14a
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-05 19:24:50 +1000

    Resolved the merge conflict and Added the adminUserDetailsUpdate function stub

--- src/auth.js
+++ src/auth.js
@@ -15,8 +15,21 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
 }
 
 /**
-<<<<<<< HEAD
- * 
+* Given a registered user's email and password returns their authUserId value.
+* @param {string} email - auth email
+* @param {string} password - auth password
+* 
+* @returns {{authUserId}} - return object
+*/
+
+function adminAuthLogin(email, password) {
+ return {
+   authUserId: 1,
+ }
+}
+
+/**
+ * Given an admin user's authUserId and a set of properties, update the properties of this logged in admin user.
  * @param {number} authUserId - the user id of the author
  * @param {string} email  - email of the author
  * @param {string} nameFirst - the first name of the author
@@ -24,20 +37,7 @@ function adminAuthRegister(email, password, nameFirst, nameLast) {
  * @returns {{}} - empty object
  */
 function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
-    return {
-
-    }
-=======
- * Given a registered user's email and password returns their authUserId value.
- * @param {string} email - auth email
- * @param {string} password - auth password
- * 
- * @returns {{authUserId}} - return object
- */
-
-function adminAuthLogin(email, password) {
   return {
-    authUserId: 1,
+
   }
->>>>>>> 468abef776926b0470e15fa3f7a4bf7ca210a9e6
 }
