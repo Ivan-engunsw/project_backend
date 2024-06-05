9788fe58784b26aaf716b35e876b73165cab28cf
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-05 19:06:55 +1000

    Added the adminUserDetailsUpdate function stub

--- src/auth.js
+++ src/auth.js
@@ -1,4 +1,3 @@
-
 /**
  * Register a user with an email, password, and names, then returns their authUserId value.
  * @param {string} email - auth email
@@ -12,5 +11,19 @@
 function adminAuthRegister(email, password, nameFirst, nameLast) {
     return {
       authUserId: 1,
+    }
+}
+
+/**
+ * 
+ * @param {number} authUserId - the user id of the author
+ * @param {string} email  - email of the author
+ * @param {string} nameFirst - the first name of the author
+ * @param {string} nameLast  - the last name of the author
+ * @returns {{}} - empty object
+ */
+function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
+    return {
+
     }
 }
