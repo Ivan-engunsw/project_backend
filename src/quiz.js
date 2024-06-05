808ef452586dee5687ec598e5bcb4e503c84e410
Dzan Gwan Alan Au <z5308353@ad.unsw.edu.au>
2024-06-05 19:06:44 +1000

    Implemented stub function for adminQuizDescriptionUpdate in quiz.js

--- src/quiz.js
+++ src/quiz.js
@@ -3,10 +3,23 @@
  *
  * @param {int} authUserId - authorised user Id
  * @param {int} quizId - quiz Id
- * @param {string} name - name of quiz
+ * @param {string} name - new name of quiz
  * @returns {{}} - empty object
  */
 function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {
+    };
+}
+
+/**
+ * Update the description of the relevant quiz.
+ * 
+ * @param {int} authUserId - authorised user Id
+ * @param {int} quizId - quiz Id
+ * @param {string} description - new description of quiz
+ * @returns {{}} - empty object
+ */
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
     return {
     };
 }
