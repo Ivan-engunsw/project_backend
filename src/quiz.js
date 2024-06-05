a02c0b94c5fb9e092248a60ddc9f317cc9300d89
z5479195 <z5479195@ad.unsw.edu.au>
2024-06-05 19:01:52 +1000

    added adminQuizRemove stub

---
+++ src/quiz.js
@@ -0,0 +1,9 @@
+/**
+ * Given a particular quiz, permanently remove the quiz.
+ * @param {integer} authUserId 
+ * @param {integer} quizId 
+ * @returns {object}
+ */
+function adminQuizRemove(authUserId, quizId) {
+    return {};
+}
