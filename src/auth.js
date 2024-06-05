42de557d65ec4a12c798d5355505695e7cb9ee1c
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-05 19:58:41 +1000

    added the adminUserPasswordUpdate function stub

--- src/auth.js
+++ src/auth.js
@@ -41,3 +41,16 @@ function adminUserDetailsUpdate(authUserId, email, nameFirst, nameLast) {
 
   }
 }
+
+/**
+ * 
+ * @param {number} authUserId - the user id of the author
+ * @param {string} oldPassword - the old password of the author
+ * @param {string} newPassword - the new password of the author
+ * @returns {{}} -empty object 
+ */
+function adminUserPasswordUpdate(authUserId, oldPassword, newPassword) {
+  return {
+
+  }
+}
