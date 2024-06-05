150ebd056daffc5e18f7b84e15ba67b8dff77a30
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-06-05 19:38:51 +1000

    fixed data type of authUserId variable

--- src/auth.js
+++ src/auth.js
@@ -31,7 +31,7 @@ function adminAuthLogin(email, password) {
 /**
  * Given an admin user's authUserId, return details about the user.
     "name" is the first and last name concatenated with a single space between them.
- * @param {integer} authUserId - auth email
+ * @param {number} authUserId - auth email
  * 
  * @returns {{user}} - return object
  */
