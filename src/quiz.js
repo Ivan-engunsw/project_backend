6c0bd367a83cf0f4db5d84fb7d1031e211ec1c90
z5479195 <z5479195@ad.unsw.edu.au>
2024-06-06 16:16:07 +1000

    added parameter and return comments to adminQuizRemove

--- src/quiz.js
+++ src/quiz.js
@@ -1,9 +1,49 @@
 /**
  * Given a particular quiz, permanently remove the quiz.
- * @param {integer} authUserId 
- * @param {integer} quizId 
- * @returns {object}
+ * @param {number} authUserId - authorised user Id
+ * @param {number} quizId - quiz Id
+ * @returns {{}} - return object
  */
 function adminQuizRemove(authUserId, quizId) {
     return {};
+}
+
+/**
+ * Get all of the relevant information about the current quiz.
+ * @param {number} authUserId - authorised user Id
+ * @param {number} quizId - quiz Id
+ * @returns {{quizInfo}} - return object
+ */
+function adminQuizInfo(authUserId, quizId) {
+    return {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+    }
+}
+
+/**
+ * Update the name of the relevant quiz.
+ * @param {number} authUserId - authorised user Id
+ * @param {number} quizId - quiz Id
+ * @param {string} name - new name of quiz
+ * @returns {{}} - empty object
+ */
+function adminQuizNameUpdate(authUserId, quizId, name) {
+    return {
+    };
+}
+
+/**
+ * Update the description of the relevant quiz.
+ * @param {int} authUserId - authorised user Id
+ * @param {int} quizId - quiz Id
+ * @param {string} description - new description of quiz
+ * @returns {{}} - empty object
+ */
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+    return {
+    };
 }
