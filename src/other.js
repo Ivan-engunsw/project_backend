b729298d8df8e547b8f8925fddac3ef63838f3d6
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-18 18:56:56 +1000

    Improved the fucntionality of the clear function by shortening the code

--- src/other.js
+++ src/other.js
@@ -1,13 +1,11 @@
-import { getData, setData } from './dataStore.js'
+import { setData } from './dataStore.js'
 /**
  * Reset the state of the application back to the start.
  * @returns {{}} - empty object
  */
 function clear() {
-    let data = getData();
-    setData(data = {
-        users: [],
-        quizzes: [],
+    setData({users: [],
+             quizzes: [],
     });
     return {      
     };
