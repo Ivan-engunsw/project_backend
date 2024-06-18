8108d6896b27c3d9976b7e213c32ac698142c77f
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-18 18:04:09 +1000

    Added a test for cheking if the state was reset

--- src/other.test.js
+++ src/other.test.js
@@ -1,6 +1,7 @@
 //The test for the other.js program which only contains the clear function
 
 import { clear } from './other.js';
+import { getData, setData } from './dataStore.js';
 
 describe('clear', () => {
     // Test which ensure that clear return an empty object
@@ -14,5 +15,22 @@ describe('clear', () => {
         const result1 = clear();
         const result2 = clear();
         expect(result1).not.toBe(result2); 
+    });
+
+    test('Clear function resets the state', () => {
+        //Setting some initial data
+        setData({ users: [{ UserId: 1, name: 'Betty', email: 'bettybooptest@gmail.com' }],
+                 quizzes: [{ id: 1, name: 'Quiz1' }] });
+    
+        let data = getData();
+        expect(data).toEqual({ users: [{ UserId: 1, name: 'Betty', email: 'bettybooptest@gmail.com' }],
+                                 quizzes: [{ id: 1, name: 'Quiz1' }] });
+    
+        //Calling the clear function
+        clear();
+    
+        // Verifying if the data was reset
+        data = getData();
+        expect(data).toEqual({ users: [], quizzes: [] });
     });
 });
