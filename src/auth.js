80b110a84d78838b8685a80cf41f0a9fa5e1bb2b
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-06-19 17:15:29 +1000

    Fixing regex statement for regexName

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,5 @@
-import { getData, setData } from './dataStore.js'
 import isEmail from 'validator/lib/isEmail';
+import { getData, setData } from './dataStore.js'
 
 /**
  * Register a user with an email, password, and names, then returns their authUserId value.
@@ -14,53 +14,34 @@ import isEmail from 'validator/lib/isEmail';
 export function adminAuthRegister(email, password, nameFirst, nameLast) {
   let data = getData();
 
-  // let regexName = /^[a-zA-Z'-\s].{2,20}$/
+  
+  let regexName = /^[a-zA-Z' -]{2,20}$/
   let regexPass = /^(?=.*?[a-zA-Z])(?=.*?[0-9]).{8,}$/
 
-  /// let isValidName = (name) => regexName.test(name);
+  let isValidName = (name) => regexName.test(name);
   let isValidPass = (pass) => regexPass.test(pass);
-
-  let isValidLength = (name) => (name.length >= 2 && name.length <= 20);
-  let isValidName = (name) => (/^[a-zA-Z'-\s]*$/.test(name))
-  
   
   if (!isEmail(email))
-    return { error: 'Email address is invalid' }
-
+    return { error: 'Invalid email' }
   if (data.users.some((user) => user.email === email))
     return { error: 'Email already registered' };
-
-
-  if (!isValidLength(nameFirst)) 
-    return { error: 'Invalid first name length' }
-  if (!isValidLength(nameLast)) 
-    return { error: 'Invalid last name length' }
-  if (!isValidName(nameFirst))
-    return { error: 'First name has invalid characters' }
-  if (!isValidName(nameLast))
-    return { error: 'Last name has invalid characters' }
   
-  /*
   if (!isValidName(nameFirst))
     return { error: 'Invalid first name' }
   if (!isValidName(nameLast))
     return { error: 'Invalid last name' }
-*/
 
   if (!isValidPass(password)) 
     return { error: 'Invalid password' }
 
-
   let authUserId = data.users.length;
   
-    
   data.users.push({
     userId: authUserId,
     name: nameFirst + ' ' + nameLast,
     email: email,
     password: password,
   })
-
 
   setData(data);
   
