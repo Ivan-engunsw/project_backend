cc020c253fe4b0c83bcd275bb480a43b787a6586
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-06-19 20:04:32 +1000

    Blackboxed the testcases for better code and use

--- src/adminUserPasswordUpdate.test.js
+++ src/adminUserPasswordUpdate.test.js
@@ -1,6 +1,8 @@
 import { clear } from './other.js';
 import { adminAuthRegister, adminUserPasswordUpdate } from './auth.js';
 
+const ERROR = { error: expect.any(String)};
+
 beforeEach(() => {
     clear();
 });
@@ -18,19 +20,19 @@ describe('adminUserPasswordUpdate', () => {
     describe('Error Testing', () => {
         test('Case when authUserId is not valid', () => {
             const changed = adminUserPasswordUpdate(9999, '1234zyx#@', 'newpass1');
-            expect(changed).toStrictEqual({ error: 'AuthUserId is not a valid user.' });
+            expect(changed).toStrictEqual(ERROR);
         });
     
         test('Case when old password is incorrect', () => {
             const { authUserId } = adminAuthRegister('originalemail@gmail.com', '1234zyx#@', 'Betty', 'Boop');
             const changed = adminUserPasswordUpdate(authUserId, 'wrongpassword', 'newpass1');
-            expect(changed).toStrictEqual({ error: 'Old Password is not the correct old password.' });
+            expect(changed).toStrictEqual(ERROR);
         });
     
         test('Case when old and new passwords match', () => {
             const { authUserId } = adminAuthRegister('originalemail@gmail.com', '1234zyx#@', 'Betty', 'Boop');
             const changed = adminUserPasswordUpdate(authUserId, '1234zyx#@', '1234zyx#@');
-            expect(changed).toStrictEqual({ error: 'Old Password and New Password match exactly.' });
+            expect(changed).toStrictEqual(ERROR);
         });
 
         test('Case when new password has already been used before', () => {
@@ -38,19 +40,19 @@ describe('adminUserPasswordUpdate', () => {
             adminUserPasswordUpdate(authUserId, '1234zyx#@', 'newpass1');
             adminUserPasswordUpdate(authUserId, 'newpass1', '12345abc');
             const changed = adminUserPasswordUpdate(authUserId, '12345abc', 'newpass1');
-            expect(changed).toStrictEqual({ error: 'New Password has already been used before by this user.' });
+            expect(changed).toStrictEqual(ERROR);
         });
     
         test('Case when new password is less than 8 characters', () => {
             const { authUserId } = adminAuthRegister('originalemail@gmail.com', '1234zyx#@', 'Betty', 'Boop');
             const changed = adminUserPasswordUpdate(authUserId, '1234zyx#@', 'short');
-            expect(changed).toStrictEqual({ error: 'New Password is less than 8 characters.' });
+            expect(changed).toStrictEqual(ERROR);
         });
     
         test('Case when new password does not contain at least one number and one letter', () => {
             const { authUserId } = adminAuthRegister('originalemail@gmail.com', '1234zyx#@', 'Betty', 'Boop');
             const changed = adminUserPasswordUpdate(authUserId, '1234zyx#@', 'abcdefgh');
-            expect(changed).toStrictEqual({ error: 'New Password does not contain at least one number and at least one letter.' });
+            expect(changed).toStrictEqual(ERROR);
         });       
     });
 
