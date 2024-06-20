2c68d06cb0cc24ec0cce61e89f86bb54f9e27efc
Dzan Gwan Alan Au <z5308353@ad.unsw.edu.au>
2024-06-20 18:33:49 +1000

    Added previous iteration of adminQuizNameUpdate to see version history

--- src/quiz.js
+++ src/quiz.js
@@ -150,23 +150,25 @@ export function adminQuizInfo(authUserId, quizId) {
  * @param {string} name - new name of quiz
  * @returns {{}} - empty object
  */
-export function adminQuizNameUpdate(authUserId, quizId, name) {
+
+function adminQuizNameUpdate(authUserId, quizId, name) {
     // Check the name provided
     if (name.length < 3 || name.length > 30) {
         return { error: `${name} is of invalid length` };
     }
 
-    let regexName = /^[a-zA-Z0-9 ]{3,30}$/
-    if(!(regexName.test(name))) {
-        return { error: `${name} is not alphanumeric`};
+    for (let char of name) {
+        if ( !((char > 'a' && char < 'z') || (char > 'A' && char < 'Z') || (char > '0' && char < '9')) ) {
+            return { error: `${name} is not alphanumeric` }
+        }
     }
 
-    let dataStore = getData();
+    let dataStore = getdata();
 
     // Check the user exists
     let user = dataStore.users.find((user) => user.userId === authUserId);
     if (!user) {
-        return { error: `authUserId = ${authUserId} not found` };
+        return { error: `authUserId = ${autherUserId} not found` };
     }
 
     // Check the quiz exists
