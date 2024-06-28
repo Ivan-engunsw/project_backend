29212ac872b06cc7bd48d9cdd976f5ce4726b0ec
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-06-28 15:38:55 +1000

    Seperated AuthLogin, AuthRegister and QuizRemove, QuizInfo test cases into individual files

--- src/adminQuizInfo.test.js
+++ src/adminQuizInfo.test.js
@@ -1,4 +1,4 @@
-import { adminQuizCreate, adminQuizRemove, adminQuizInfo } from "./quiz";
+import { adminQuizCreate, adminQuizInfo } from "./quiz";
 import { adminAuthRegister } from "./auth";
 import { clear } from "./other";
 
@@ -61,47 +61,5 @@ describe("adminQuizInfo", () => {
             timeLastEdited: expect.any(Number),
             description: "desc"
         });
-    });
-});
-
-describe("adminQuizRemove", () => {
-    let authUserId1, authUserId2, quizId1, quizId2, quizId3, quizId4
-    beforeEach(() => {
-        clear();
-        authUserId1 = adminAuthRegister("auth@one.com", "authone1", "auth", "one").authUserId;
-        authUserId2 = adminAuthRegister("auth@two.com", "authtwo2", "auth", "two").authUserId;
-        quizId1 = adminQuizCreate(authUserId1, "first", "desc").quizId;
-        quizId2 = adminQuizCreate(authUserId1, "second", "desc").quizId;
-        quizId3 = adminQuizCreate(authUserId1, "third", "desc").quizId;
-        quizId4 = adminQuizCreate(authUserId2, "name", "desc").quizId;
-    });
-
-    test("AuthUserId is not a valid user", () => {
-        expect(adminQuizRemove("", quizId1)).toStrictEqual(ERROR);
-    });
-
-    test("Quiz ID does not refer to a valid quiz", () => {
-        expect(adminQuizRemove(authUserId1, "")).toStrictEqual(ERROR);
-    });
-
-    test("Quiz ID does not refer to a quiz that this user owns", () => {
-        expect(adminQuizRemove(authUserId1, quizId4)).toStrictEqual(ERROR);
-    });
-
-    test("Correct return type", () => {
-        expect(adminQuizRemove(authUserId1, quizId1)).toStrictEqual({});
-    });
-
-    test("Successfully delete one quiz", () => {
-        adminQuizRemove(authUserId1, quizId1);
-        expect(adminQuizCreate(authUserId1, "first", "desc").quizId).toStrictEqual(expect.any(Number));
-    });
-
-    test("Successfully delete multiple quizzes", () => {
-        adminQuizRemove(authUserId1, quizId1);
-        expect(adminQuizCreate(authUserId1, "first", "desc").quizId).toStrictEqual(expect.any(Number));
-
-        adminQuizRemove(authUserId1, quizId3);
-        expect(adminQuizCreate(authUserId1, "third", "desc").quizId).toStrictEqual(expect.any(Number));
     });
 });
