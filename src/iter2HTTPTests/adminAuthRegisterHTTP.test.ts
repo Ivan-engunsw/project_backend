090b43f7add1a940654a5285eb2c6ff666011ec1
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-07-10 18:24:45 +1000

    Finalising adminAuthRegisterHTTP tests

--- src/iter2HTTPTests/adminAuthRegisterHTTP.test.ts
+++ src/iter2HTTPTests/adminAuthRegisterHTTP.test.ts
@@ -15,6 +15,16 @@ const inputUser = {
   timeout: TIMEOUT_MS
 };
 
+const inputUser2 = {
+  json: {
+    email: 'validemail2@gmail.com',
+    password: 'password1!',
+    nameFirst: 'Bobby',
+    nameLast: 'Bob'
+  },
+  timeout: TIMEOUT_MS
+};
+
 beforeEach(() => {
   request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
 });
@@ -92,12 +102,18 @@ describe('POST /v1/admin/auth/register', () => {
   });
 
   describe('Functionality testing', () => {
-    test('Has the correct return type', () => {
+    test('Has the correct return type for one user', () => {
       const res = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser);
       expect(JSON.parse(res.body.toString())).toStrictEqual({ token: expect.any(String) });
     });
 
-    /*
+    test('Has the correct return type for multiple user', () => {
+      const res = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser);
+      const res2 = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser2);
+      expect(JSON.parse(res.body.toString())).toStrictEqual({ token: expect.any(String) });
+      expect(JSON.parse(res2.body.toString())).not.toStrictEqual(res.body.toString());
+    });
+
     test('Successfully creates a user', () => {
       const res = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser);
       const token: { token: string } = JSON.parse(res.body.toString());
@@ -114,6 +130,36 @@ describe('POST /v1/admin/auth/register', () => {
         }
       });
     });
-    */
+
+    test('Successfully creates multiple users', () => {
+      const res = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser);
+      const res2 = request('POST', SERVER_URL + '/v1/admin/auth/register', inputUser2);
+      const token: { token: string } = JSON.parse(res.body.toString());
+      const token2: { token: string } = JSON.parse(res2.body.toString());
+
+      const desc = request('GET', SERVER_URL + '/v1/admin/user/details',
+        { qs: { token: token.token }, timeout: TIMEOUT_MS });
+      expect(JSON.parse(desc.body.toString())).toStrictEqual({
+        user: {
+          userId: expect.any(Number),
+          name: 'Bobby Bob',
+          email: 'validemail1@gmail.com',
+          numSuccessfulLogins: expect.any(Number),
+          numFailedPasswordsSinceLastLogin: expect.any(Number)
+        }
+      });
+
+      const desc2 = request('GET', SERVER_URL + '/v1/admin/user/details',
+        { qs: { token: token2.token }, timeout: TIMEOUT_MS });
+      expect(JSON.parse(desc2.body.toString())).toStrictEqual({
+        user: {
+          userId: expect.any(Number),
+          name: 'Bobby Bob',
+          email: 'validemail2@gmail.com',
+          numSuccessfulLogins: expect.any(Number),
+          numFailedPasswordsSinceLastLogin: expect.any(Number)
+        }
+      });
+    });
   });
 });
