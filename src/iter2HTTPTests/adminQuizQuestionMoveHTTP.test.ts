35893b153e499fcc71d2d88cf6c88c6f6a6f66c0
Ivan Lun Hui Chen <z5557064@ad.unsw.edu.au>
2024-07-11 17:01:01 +1000

    Written HTTP tests for adminQuizQuestionMove

---
+++ src/iter2HTTPTests/adminQuizQuestionMoveHTTP.test.ts
@@ -0,0 +1,157 @@
+import request from 'sync-request-curl';
+import { port, url } from '../config.json';
+import { timeNow } from '../helper';
+
+const ERROR = { error: expect.any(String) };
+const SERVER_URL = `${url}:${port}`;
+const TIMEOUT_MS = 5 * 1000;
+
+beforeEach(() => {
+  request('DELETE', SERVER_URL + '/v1/clear', { timeout: TIMEOUT_MS });
+});
+
+describe('PUT /v1/admin/quiz/:quizId/question/:questionId/move', () => {
+    test('Token is non-existent', () => {
+    const res = request('PUT', SERVER_URL + '/v1/admin/quiz/0/question/0/move', { json: { token: '0', newPosition: 1 }, timeout: TIMEOUT_MS });
+    expect(JSON.parse(res.body.toString())).toStrictEqual(ERROR);
+    expect(res.statusCode).toStrictEqual(401);
+    });
+
+    describe('After creating tokens', () => {
+      let token: {token: string};
+      beforeEach(() => {
+          const res = request('POST', SERVER_URL + '/v1/admin/auth/register',
+          {
+              json: {
+              email: 'bettyBoop@gmail.com',
+              password: 'helloWorld1',
+              nameFirst: 'Betty',
+              nameLast: 'Boop'
+              },
+              timeout: TIMEOUT_MS
+          });
+          token = JSON.parse(res.body.toString());
+      });
+
+      test('Token is not a valid user', () => {
+          const res1 = request('PUT', SERVER_URL + '/v1/admin/quiz/0/question/0/move',
+          { json: { token: token.token + '1',  newPosition: 1}, timeout: TIMEOUT_MS });
+          expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+          expect(res1.statusCode).toStrictEqual(401);
+      });
+
+      describe('After creating quizzes', () => {
+        let quiz1: { quizId: number};
+        beforeEach(() => {
+          const quizRes1 = request('POST', SERVER_URL + '/v1/admin/quiz',
+            {
+              json: {
+                token: token.token,
+                name: 'quiz 1',
+                description: 'Mathematics Quiz'
+              },
+              timeout: TIMEOUT_MS
+            });
+          quiz1 = JSON.parse(quizRes1.body.toString());
+        });
+
+        test('QuizId does not exist', () => {
+          const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId+1}/question/0/move`,
+            { json: { token: token.token,  newPosition: 1}, timeout: TIMEOUT_MS });
+            expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+            expect(res1.statusCode).toStrictEqual(403);
+        });
+
+        test('User is not an owner of the quiz', () => {
+          const resUser2 = request('POST', SERVER_URL + '/v1/admin/auth/register', 
+            { json: { email: 'auth@two.com', password: 'authtwo2', nameFirst: 'auth', nameLast: 'two' }, timeout: TIMEOUT_MS });
+          const token2 = JSON.parse(resUser2.body.toString());
+          const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/0/move`,
+            { json: { token: token2.token,  newPosition: 1}, timeout: TIMEOUT_MS });
+          expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+          expect(res1.statusCode).toStrictEqual(403);
+        });
+
+        describe('After creating questions', () => {
+          let questionId: {questionId: number};
+          beforeEach(() => {
+            const questionRes = request('POST', SERVER_URL + `v1/admin/quiz/${quiz1.quizId}/question`, 
+              {json: {token: token.token, questionBody: {question: "Who is the Monarch of England", duration: 4, points: 5,
+                answers: [{answer: "Prince Charles", correct: true}]
+              }}});
+            questionId = JSON.parse(questionRes.body.toString());
+          });
+
+          test('QuestionId is not valid', () => {
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId+1}/move`,
+              { json: { token: token.token,  newPosition: 1}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+              expect(res1.statusCode).toStrictEqual(400);
+          });
+
+          test('NewPosition is less than 0', () => {
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId}/move`,
+              { json: { token: token.token,  newPosition: -1}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+              expect(res1.statusCode).toStrictEqual(400);
+          });
+
+          test('NewPosition is greater than no of questions', () => {
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId}/move`,
+              { json: { token: token.token,  newPosition: 2}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+              expect(res1.statusCode).toStrictEqual(400);
+          });
+
+          test('NewPosition is the position of the current question', () => {
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId}/move`,
+              { json: { token: token.token,  newPosition: 0}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual(ERROR);
+              expect(res1.statusCode).toStrictEqual(400);
+          });
+
+          test('Returns correct output', () => {
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId}/move`,
+              { json: { token: token.token,  newPosition: 1}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual({});
+          });
+
+          test('Correctly moves a question', () => {
+            const questionRes2 = request('POST', SERVER_URL + `v1/admin/quiz/${quiz1.quizId}/question`, 
+              {json: {token: token.token, questionBody: {question: "Who is Ronaldo", duration: 4, points: 5,
+                answers: [{answer: "Football player", correct: true}]
+              }}});
+            const questionId2 = JSON.parse(questionRes2.body.toString());
+            const res1 = request('PUT', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}/question/${questionId.questionId}/move`,
+              { json: { token: token.token,  newPosition: 1}, timeout: TIMEOUT_MS });
+              expect(JSON.parse(res1.body.toString())).toStrictEqual({});
+
+            const quizRes2 = request('GET', SERVER_URL + `/v1/admin/quiz/${quiz1.quizId}`, { qs: { token: token.token }, timeout: TIMEOUT_MS });
+            const quiz = JSON.parse(quizRes2.body.toString());
+            expect(quiz.questions).toStrictEqual([
+              {
+                questionId: questionId2.questionId,
+                question: "Who is Ronaldo",
+                duration: 4,
+                points: 5,
+                answers: [{answerId: expect.any(Number), answer: "Football player", colour: expect.any(String), correct: true}]
+              },
+              {
+                questionId: questionId.questionId,
+                question: "Who is the Monarch of England",
+                duration: 4,
+                points: 5,
+                answers: [{answerId: expect.any(Number), answer: "Prince Charles", colour: expect.any(String), correct: true}]
+              }
+            ]);
+
+            const time = timeNow();
+            const timeEditedRes = request('GET', SERVER_URL + `/v1/admin/quiz/${quiz.quizId}`, { qs: { token: token.token }, timeout: TIMEOUT_MS });
+            const timeLastEdited = parseInt(JSON.parse(timeEditedRes.body.toString()).timeLastEdited);
+            expect(timeLastEdited).toBeGreaterThanOrEqual(time);
+            expect(timeLastEdited).toBeLessThanOrEqual(time + 1);
+          });
+        });
+      });
+    });
+});
