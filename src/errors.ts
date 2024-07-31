c674dc5af84ea8a10adf1a2bbb40b7ba02c454f4
Stephen Gottlieb <z5481352@ad.unsw.edu.au>
2024-07-31 23:21:23 +1000

    Modifying sendChatMessage function to use new helperFunction and error message in error.ts and helper.ts

--- src/errors.ts
+++ src/errors.ts
@@ -94,8 +94,7 @@ export const invalidState = (state: State) =>
 export const nameTaken = (name: string) =>
   `The name: ${name} is already taken by another player`;
 export const playerIdNotFound = (playerId: number) =>
-  `The playerID: ${playerId} does not exist`;
-
+  `The playerId: ${playerId} does not exist`;
 
 export const invalidMessageLength = () =>
   'The message needs to be at least 1 character and no more than 100 characters';
