a3dc2574c8026ac725cc5079168fd059332cdece
Shubham Pratik <z5520538@ad.unsw.edu.au>
2024-08-02 13:38:08 +1000

    Added three more error helps regarding player

--- src/errors.ts
+++ src/errors.ts
@@ -99,3 +99,9 @@ export const sessionsNotEnded = () =>
 // players
 export const nameTaken = (name: string) =>
   `The name: ${name} is already taken by another player`;
+export const playerIdNotFound = (playerId: number) =>
+  `The playerId: ${playerId} does not exist`;
+export const invalidMessageLength = () =>
+  'The message needs to be at least 1 character and no more than 100 characters';
+export const invalidPlayer = (playerId: number) =>
+  `The playerId: ${playerId} is invalid`;
